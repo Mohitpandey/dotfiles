a2ec0393e781928a9f25ace562ceff605783f1ce
Mohit Pandey <mohitpandey.all@gmail.com>
2014-08-29 19:24:35 -0700

    updating setup to clone repo

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #/bin/bash
 
-source config.sh
+export DOTFILES="$HOME/test/.dotfiles"
+export HOMEBREW="$DOTFILES/homebrew"
 
-ln -s $DOTFILES/.oh-my-zsh.symlink $HOME/.oh-my-zsh
-ln -s $DOTFILES/.zshrc.symlink $HOME/.zshrc
+xcode-select --install
 
-ln -s $DOTFILES/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
+mkdir -p $HOMEBREW && curl -L https://github.com/Homebrew/homebrew/tarball/master | tar xz --strip 1 -C $HOMEBREW
+
+git clone https://github.com/Mohitpandey/dotfiles.git $DOTFILES
+
+export $PATH="$HOMEBREW/bin:$PATH"
+
+#ln -s $DOTFILES/.oh-my-zsh.symlink $HOME/.oh-my-zsh
+#ln -s $DOTFILES/.zshrc.symlink $HOME/.zshrc
+#ln -s $DOTFILES/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
