1582e9d07de40ae12a831081e711ceaa47b093ca
Mohit <mohitpandey.all@gmail.com>
2014-08-29 20:44:19 -0400

    added symlinking iterm2 settings

--- setup.sh
+++ setup.sh
@@ -5,3 +5,4 @@ source config.sh
 ln -s $DOTFILES/.oh-my-zsh.symlink $HOME/.oh-my-zsh
 ln -s $DOTFILES/.zshrc.symlink $HOME/.zshrc
 
+ln -s $DOTFILES/com.googlecode.iterm2.plist ~/Library/Preferences/com.googlecode.iterm2.plist
