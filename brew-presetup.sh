4e4f56e3f113812f4238c3daa0b26de2501abf16
Mohit <Mohitpandey@users.noreply.github.com>
2018-11-22 18:35:54 -0500

    Update brew-presetup.sh

--- brew-presetup.sh
+++ brew-presetup.sh
@@ -1,4 +1,6 @@
-# rather than giving sudo to brew install script, I am setting up things here (yes, I am being security paranoid)
+#!/usr/bin/env bash
+
+# rather than giving sudo to brew install script, I am setting up things here (yes, I am being security paranoid). 
 /usr/bin/sudo /bin/mkdir -p /usr/local/Homebrew /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/sbin /usr/local/share /usr/local/var /usr/local/opt /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew /usr/local/var/homebrew/linked /usr/local/Cellar /usr/local/Caskroom /usr/local/Frameworks
 /usr/bin/sudo /bin/chmod g+rwx /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/sbin /usr/local/share /usr/local/var /usr/local/opt /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var/homebrew /usr/local/var/homebrew/linked /usr/local/Cellar /usr/local/Caskroom /usr/local/Frameworks
 /usr/bin/sudo /bin/chmod 755 /usr/local/share/zsh /usr/local/share/zsh/site-functions
